c49152eab9821060e4cd82ad55ae324c9fdef227
fabrice-rw <fabricenzayisenga@gmail.com>
2022-08-27 10:37:05 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Main - the program prints _putchar to standard output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
